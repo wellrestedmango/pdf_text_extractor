c12270278be32b1127903dcd964c7b6e3afd9322
wellrestedmango <156558082+wellrestedmango@users.noreply.github.com>
2025-04-16 01:43:36 +0000

    Update main.py
    adding in stats of interest, cleaning up the way that found files are saved to the dictionary. Next up is to find out how to export that dict to a csv to be usable. Maybe output stats of interest to a txt file at the end to save that

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
 import os
 import glob
-import re
 import argparse
-import pymupdf
-from pymupdf import get_text
 import PyPDF2
 from PyPDF2 import PdfReader
 from argparse import ArgumentParser
@@ -19,18 +16,22 @@ dst_dir = args.dst
 
 search_terms = ["social", "Social", "media", "Media", "Mastodon", "mastodon"]
 
-interesting_files = []
+interesting_files = {}
+failed_files = []
 
-file_dict = {}
+failed_files_count = 0
+found_boring = 0
+found_interesting = 0
+
+
 
 for p in glob.glob('**', recursive=True, root_dir=src_dir):
     if os.path.isfile(os.path.join(src_dir, p)):
         file_path = f"{os.path.join(src_dir, p)}"
         print(file_path)
-        file_dict[p] = []
-        #with open(file_path, 'r') as pdfFileObj:
         text = ""
         try:
+            found_terms = []
             doc = PdfReader(file_path)  # open a document
             for page in range(0, len(doc.pages)):  # iterate the document pages
                 curr_page = doc.pages[page]
@@ -38,14 +39,26 @@ for p in glob.glob('**', recursive=True, root_dir=src_dir):
             for term in search_terms:
                 if term in text:
                     print(f"Found {term} in {p}")
-                    file_dict[p].append(term)
+                    found_terms.append(term)
                     break
                 else:
                     print("huge bust")
+            if not found_terms:
+                found_boring += 1
+                pass
+            else:
+                found_interesting += 1
+                interesting_files.update({p:found_terms})
+                print("still working Eric, don't worry")
         except:
             print("failed")
+            failed_files.append(p)
+            failed_files_count += 1
 
-print(file_dict)
+print(interesting_files)
+print(f'Number of failed files: {failed_files_count}')
+print(f'Number of boring files: {found_boring}')
+print(f'Number of interesting files: {found_interesting}')
 
 
 
