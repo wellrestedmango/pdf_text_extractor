7e4b1d0375de0f6a66addc511b653e4dfe2dd7b8
wellrestedmango <156558082+wellrestedmango@users.noreply.github.com>
2025-04-16 01:09:30 +0000

    Update main.py
    getting it closer to the final stages. Currently deciding which pdf reader I want to use. Search is working, trying to save data to a df I may be able to output to csv.

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 import os
 import glob
-import PyPDF3
 import re
 import argparse
+import pymupdf
+from pymupdf import get_text
+import PyPDF2
+from PyPDF2 import PdfReader
 from argparse import ArgumentParser
 
 
@@ -16,28 +19,33 @@ dst_dir = args.dst
 
 search_terms = ["social", "Social", "media", "Media", "Mastodon", "mastodon"]
 
+interesting_files = []
+
+file_dict = {}
+
 for p in glob.glob('**', recursive=True, root_dir=src_dir):
     if os.path.isfile(os.path.join(src_dir, p)):
         file_path = f"{os.path.join(src_dir, p)}"
         print(file_path)
+        file_dict[p] = []
         #with open(file_path, 'r') as pdfFileObj:
         text = ""
         try:
-            pdfReader = PyPDF3.PdfFileReader(file_path)
-            for i in range(pdfReader.numPages):
-                page = pdfReader.pages[i]
-                text += f", {page.extractText()}"
-
-            sentence_text = text.split(",")
-
-            for sentence in sentence_text:
-                for keyword in search_terms:
-                    if keyword in sentence:
-                        print(f'keyword: {keyword} found in sentence: {sentence}')
-
+            doc = PdfReader(file_path)  # open a document
+            for page in range(0, len(doc.pages)):  # iterate the document pages
+                curr_page = doc.pages[page]
+                text += curr_page.extract_text()# get plain text (is in UTF-8)
+            for term in search_terms:
+                if term in text:
+                    print(f"Found {term} in {p}")
+                    file_dict[p].append(term)
+                    break
+                else:
+                    print("huge bust")
         except:
-            print("it didn't work")
+            print("failed")
 
+print(file_dict)
 
 
 
